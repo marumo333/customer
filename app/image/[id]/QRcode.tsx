311e361022f448840c3f0d51ab808022d7757db8
marumo333 <seito5026junior@icloud.com>
2025-05-06 04:24:02 +0900

    商品の詳細ページを追加

---
+++ app/image/[id]/QRcode.tsx
@@ -0,0 +1,29 @@
+"use client"
+import React from "react";
+import {FC} from "react";
+import { QRCodeCanvas } from "qrcode.react";
+
+interface QRcodeProps{
+    url:string,
+}
+
+const QRcode:FC<QRcodeProps>=(props)=>{
+    return (
+        <QRCodeCanvas
+          value={props.url}
+          size={128}
+          bgColor={"#FF0000"}
+          fgColor={"#FFC0CB"}
+          level={"L"}
+          imageSettings={{
+            src: "/favicon.ico",
+            x: undefined,
+            y: undefined,
+            height: 24,
+            width: 24,
+            excavate: true,
+          }}
+        />
+      );
+}
+export default QRcode;

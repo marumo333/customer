a25f577f1f88940ff483a09ed1319f6f1eeb655d
marumo333 <seito5026junior@icloud.com>
2025-05-08 15:15:18 +0900

    Like表示機能を追加

---
+++ app/myPage/likes.tsx
@@ -0,0 +1,71 @@
+"use client"
+import { supabase } from "@/utils/supabase/supabase";
+import React, { useState, useEffect } from "react";
+
+interface LikesImage {
+    image_url: string;
+    image_id: string;
+}
+
+interface Likes {
+    userId: string;
+}
+
+export default function Like({ userId }: Likes) {
+    const [likedImages, setLikedImages] = useState<LikesImage[]>([])
+    const [isLiked, setIsLiked] = useState(false)
+
+    useEffect(() => {
+        const fetchLikes = async () => {
+
+            try {
+                const { data, error } = await supabase
+                    .from('likes')
+                    .select("image_id,user_id,posts(id,image_url)")
+                    .eq('user_id', userId)
+                console.log(data);
+                console.log(error)
+                if (error) {
+                    console.error('いいねの投稿を取得だきませんでした', error.message);
+                    return;
+                }
+
+                if (data && data.length > 0) {
+                    const formattedData = (data as any[]).map((item) => ({
+                        image_id: item.image_id,
+                        image_url: item.posts?.image_url || "",
+                    }))
+                    setLikedImages(formattedData)
+                    setIsLiked(true)//いいね済みのものを表示
+                }
+                else {
+                    setIsLiked(false)
+                }
+            } catch (err) {
+                console.error("お気に入りの取得に失敗しました", err)
+            }
+        };
+        if (userId){ fetchLikes()
+        }
+    }, [userId])
+
+    return (
+        <div>
+            <h1>お気に入りの投稿</h1>
+            {isLiked ? (
+                likedImages.map((img) => (
+                    <div key={img.image_id}>
+                        <p>画像ID:{img.image_id}</p>
+                        {img.image_url ? (
+                            <img src={img.image_url} alt="お気に入りの画像" width={200} />
+                        ) : (
+                            <p>画像が見つかりません</p>
+                        )}
+                    </div>
+                ))
+            ) : (
+                <p>いいねした投稿はありません</p>
+            )}
+        </div>
+    )
+}
